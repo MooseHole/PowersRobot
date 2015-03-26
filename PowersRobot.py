89c443f54dfb4201b6e6130c58ce5225abc8d608
Moose Hole <moose_hole@yahoo.com>
2015-03-25 20:06:09 -0500

    Fix battle object access

--- PowersRobot.py
+++ PowersRobot.py
@@ -114,7 +114,7 @@ while True:
 				print ("Found " + powerWord + " at " + str(position) + " Begin " + str(begin) + " End: " + str(end))
 				if end > begin:
 					powerWords[powerWord](op_text[begin:end], battle)
-		if battle.name.length() > 0:
+		if len(battle.name) > 0:
 			r.send_message('Moose_Hole', 'A Battle!', battle)
 			msg.mark_as_read()
 	time.sleep(30)
