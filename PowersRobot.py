d577fbce150403da21507dd2ae91f50cbe376f55
Moose Hole <moose_hole@yahoo.com>
2015-03-29 14:38:31 -0500

    bugs

--- PowersRobot.py
+++ PowersRobot.py
@@ -34,7 +34,8 @@ battle = Battle()
 # Main loop
 while True:
 	# Check own subreddit
-	unread = r.search(subreddit:"'" + os.environ['REDDIT_USER'] + "'", title:'/r/* Settings')
+	queryString = "subreddit:'" + os.environ['REDDIT_USER'] + "', title:'/r/* Settings'"
+	unread = r.search(queryString)
 
 	# Check unread messages
 #	unread = r.get_unread(limit=None)
